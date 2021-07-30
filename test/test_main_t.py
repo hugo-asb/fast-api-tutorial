24bb6474053419a366dfbfb9eb4f2525d0a4d9c4
Hugo Barbosa <hugo.asb96@gmail.com>
2021-07-30 16:55:46 -0300

    Adding Testing guide.

---
+++ test/test_main_t.py
@@ -0,0 +1,83 @@
+from fastapi.testclient import TestClient
+from .main_t import app
+
+
+client = TestClient(app)
+
+
+'''
+All tests are run with pytest.
+Let's say you have a file main_t.py with your FastAPI app.
+It has a GET operation that could return an error.
+It has a POST operation that could return several errors.
+Both path operations require an X-Token header.
+
+Whenever you need the client to pass information in the request and you don't
+know how to, you can search (Google) how to do it in requests.
+Then you just do the same in your tests. E.g.:
+- To pass a path or query parameter, add it to the URL itself.
+- To pass a JSON body, pass a Python object (e.g. a dict) to the parameter json.
+- If you need to send Form Data instead of JSON, use the data parameter instead.
+- To pass headers, use a dict in the headers parameter.
+- For cookies, a dict in the cookies parameter.
+'''
+
+
+def test_read_item():
+    response = client.get("/items/foo", headers={"X-Token": "coneofsilence"})
+    assert response.status_code == 200
+    assert response.json() == {
+        "id": "foo",
+        "title": "Foo",
+        "description": "There goes my hero",
+    }
+
+
+def test_read_item_bad_token():
+    response = client.get("/items/foo", headers={"X-Token": "hailhydra"})
+    assert response.status_code == 400
+    assert response.json() == {"detail": "Invalid X-Token header"}
+
+
+def test_read_inexistent_item():
+    response = client.get("/items/baz", headers={"X-Token": "coneofsilence"})
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Item not found"}
+
+
+def test_create_item():
+    response = client.post(
+        "/items/",
+        headers={"X-Token": "coneofsilence"},
+        json={"id": "foobar", "title": "Foo Bar", "description": "The Foo Barters"},
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+        "id": "foobar",
+        "title": "Foo Bar",
+        "description": "The Foo Barters",
+    }
+
+
+def test_create_item_bad_token():
+    response = client.post(
+        "/items/",
+        headers={"X-Token": "hailhydra"},
+        json={"id": "bazz", "title": "Bazz", "description": "Drop the bazz"},
+    )
+    assert response.status_code == 400
+    assert response.json() == {"detail": "Invalid X-Token header"}
+
+
+def test_create_existing_item():
+    response = client.post(
+        "/items/",
+        headers={"X-Token": "coneofsilence"},
+        json={
+            "id": "foo",
+            "title": "The Foo ID Stealers",
+            "description": "There goes my stealer",
+        },
+    )
+    assert response.status_code == 400
+    assert response.json() == {"detail": "Item already exists"}
